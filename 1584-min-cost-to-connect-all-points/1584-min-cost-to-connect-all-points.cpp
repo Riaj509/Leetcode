6b78f4ef0446c9a3194439ca6947aacc62d1788f
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-08-12 00:27:10 +0600

    Time: 356 ms (68.97%), Space: 129.7 MB (29.58%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,73 +1,117 @@
-struct weight{
-    long long int w,u,v;
-};
 class Solution {
-public:    
-long long int parent[100000];
-    long long int rank[100000];
+public:
 
-    
-    int minCostConnectPoints(vector<vector<int>>& points) {
-       
-        
-        
-        vector<weight> connect;
-        int n=points.size();
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            rank[i]=0;
-        }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                long long int val=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-            connect.push_back({val,i,j});
-            }
-        }
-     sort(connect.begin(),connect.end(),[&](const weight &a ,const weight &b){
-         return a.w<b.w;
-     });
-                // for(auto x : connect)cout<<x.w<<" "<<x.u<<" "<<x.v<<endl;
-
-        long long int ans=0;
-        for(auto x : connect)
-        {
-            long long int cost=x.w;
-             int src=x.u;
-             int  des=x.v;
-            int ck,ck1;
-            ck=find(src);
-            ck1=find(des);
-            if(ck!=ck1){
-                union_(src,des);
-                ans+=cost; 
-            }
-            
-        }
-         return ans;
-        
-    }
-    
-    
-
-
-long long int  find(long long int  u)
+int dist(int x1, int y1, int x2, int y2)
 {
-    if (parent[u] == u)
-        return u;
-    return parent[u] = find(parent[u]);
+    return (abs(x1-x2) + abs(y1-y2));
 }
 
-void union_(long long int  u, long long int v)
+void buildGraph(vector<vector<int>>& points, vector<vector<pair<int, int>>> &adj)
 {
-    long long int a, b;
-    a = find(u);
-    b = find(v);
-   if(rank[a]>rank[b]) parent[b]=a;
-    else if(rank[b]>rank[a])parent[b]=a;
-    else {
-        parent[b]=a;
-        rank[a]++;
+    int V = points.size();
+    
+    for(int i=0;i<V;i++)
+    {
+        vector<pair<int,int>> temp;
+        for(int j=0;j<V;j++)
+        {
+            if(i==j)// no need to calculate the distance
+                continue;
+                
+            else
+            {
+                int src_x = points[i][0];
+                int src_y = points[i][1];
+            
+                int dest_x = points[j][0];
+                int dest_y = points[j][1];
+                
+                int wt = dist(src_x, src_y, dest_x, dest_y);
+                temp.push_back({j,wt});    
+            }
+        }
+        
+        adj.push_back(temp);
     }
-     
+}
+
+
+int minVertex(vector<int> &distance, vector<bool> &setMST)
+{
+    int minV = INT_MAX;
+    int minD = INT_MAX;
+    
+    for(int i=0;i<distance.size();i++)
+    {
+        if(setMST[i]==false && distance[i]<minD)
+        {
+            minV = i;
+            minD = distance[i];
+        }
+    }
+    
+    return minV;
+}
+    
+int MST(vector<vector<int>>& points, vector<vector<pair<int,int>>> &adj)
+{
+    int V = points.size();
+    int ans = 0;
+    
+    vector<int> distance(V,INT_MAX);
+    vector<bool> setMST(V,false);
+    distance[0] = 0;
+    
+    for(int i=0;i<V-1;i++)
+    {
+        int u = minVertex(distance,setMST);
+        setMST[u] = true;
+        
+        for(auto it : adj[u])
+        {
+            int v = it.first;// neighbour
+            int weight = it.second;// node weight
+            
+            // from the already created adjacency list , take out V and wt , if vertex is not part of MST
+            // & curr_weight < prev_weight, then include in MST
+            
+            if (setMST[v] == false && weight < distance[v])
+            {
+                distance[v] = weight;
+                // ans+=weight;
+            }
+        }  
+    }
+    
+    for(int i=0;i<distance.size();i++)
+    {
+        ans+=distance[i];
+        // cout<<distance[i]<<" ";
+    }
+            
+    return ans;
+}
+
+void printAdj(vector<vector<int>> &points, vector<vector<pair<int, int>>> &adj)
+{
+    int V = points.size();
+    for (int i = 0; i < V; i++)
+    {
+        for(auto &x : adj[i])
+            cout<<"("<<x.first<<","<<x.second<<")"<<" , ";       
+        
+        cout << endl;
+    }
+}
+
+int minCostConnectPoints(vector<vector<int>>& points)
+{
+    vector<vector<pair<int, int>>> adj;
+    buildGraph(points,adj);
+    // printAdj(points,adj);
+    // cout<<endl;
+    
+    int res = MST(points,adj);
+    return res;
 }
 };
