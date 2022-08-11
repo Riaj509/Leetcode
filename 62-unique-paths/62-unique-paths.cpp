24dbeaaf4188673fc2b0bc0dfe4f27fbedda794b
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-08-11 20:38:42 +0600

    Time: 3 ms (44.87%), Space: 5.9 MB (75.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-   int row,col;
     int uniquePaths(int m, int n) {
-    vector<vector<long long int>>dp(n+1,vector<long long int>(m+1,-1));
-        row=n,col=m;
-       return solve(0,0,dp);
+        int dp[n][m];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(i==n-1 || j==m-1)dp[i][j]=1;
+                else dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
     
-    int solve(int x,int y,vector<vector<long long int>>&dp ){
-            if(x==row-1 && y==col-1)return 1;
-            if(dp[x][y]!=-1)return dp[x][y];
-        long long int ret=0,ret1=0;
-        if(x+1<row) ret=solve(x+1,y,dp);
-        if(y+1<col)ret1=solve(x,y+1,dp);
-            return dp[x][y]=ret+ret1;
-    }
 };
