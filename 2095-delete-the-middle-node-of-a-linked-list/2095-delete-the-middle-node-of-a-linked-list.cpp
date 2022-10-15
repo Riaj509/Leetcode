30e94e33d6e90d1f96d8f6c5f7d0d0a3606aab10
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-15 17:05:27 +0600

    Time: 2203 ms (21.12%), Space: 294.7 MB (64.79%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,25 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if(head->next==nullptr)return nullptr;
+        // Edge case: return None if there is only one node
+        if (head -> next == nullptr)
+            return nullptr;
         
-        ListNode *sl=head,*fs=head->next->next;
+        int count = 0;
+        ListNode *p1 = head, *p2 = head;
         
-        while(fs!=nullptr && fs->next!=nullptr){
-            sl=sl->next;
-            fs=fs->next->next;
+        // First pass, count the number of nodes in the linked list using 'p1'.
+        while (p1 != nullptr) {
+            count++;
+            p1 = p1 -> next;
         }
-        sl->next=sl->next->next;
+        
+        // Get the index of the node to be deleted.
+        int middleIndex = count / 2;
+        
+        // Second pass, let 'p2' move toward the predecessor of the middle node.
+        for (int i = 0; i < middleIndex - 1; ++i)
+            p2 = p2 -> next;
+        
+        // Delete the middle node and return 'head'.
+        p2 -> next = p2 -> next -> next;
         return head;
     }
 };
