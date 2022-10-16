ab9a362a7e99a482c5daee4d0341c5f55918f28f
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-16 12:53:23 +0600

    Time: 38 ms (19.30%), Space: 8.2 MB (24.61%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int> trig;
+        trig['I']=1;
+        trig['V']=5;
+        trig['X']=10;
+        trig['L']=50;
+        trig['C']=100;
+        trig['D']=500;
+        trig['M']=1000;
+        int ans=0;
+        for(int i=0;i<s.size();i++ ){
+            if(i+1<s.size()){
+                if(trig[s[i]]<trig[s[i+1]]){
+                    ans+=trig[s[i+1]]-trig[s[i]];
+                    i++;
+                }
+                else ans+=trig[s[i]];
+            }
+            else  ans+=trig[s[i]];
+        }
+        return ans;
+    }
+};
