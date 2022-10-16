5018057c4316aa7619c02112de0a8f12617962f5
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-16 13:52:49 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (25.00%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+     long long  l=1,r=x;
+        while(l<=r){
+            long long  mid=l+(r-l)/2;
+            if(mid*mid== x)return mid;
+            else if(mid*mid>x)r=mid-1;
+            else l=mid+1;
+        }
+        return r;
     }
 };
