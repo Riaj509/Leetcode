0f3fb34b32619bcace2610be382c5fd6dca1f18e
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-18 14:12:54 +0600

    Time: 7 ms (6.10%), Space: 6.5 MB (30.37%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-	vector<vector<int>> ret;
-	for (int i = 0; i < numRows; i++) {
-		vector<int> row(i + 1, 1);
-		for (int j = 1; j < i; j++) {
-			row[j] = ret[i - 1][j] + ret[i - 1][j - 1];
-		}
-		ret.push_back(row);
-	}
-	return ret;
-}
+        int n=numRows;
+        vector<vector<int>>pas(n+1,vector<int>(2*n+1));
+        vector<vector<int>>ans(n);
+        
+        for(int i=1,j=n;i<=n && j>=1;i++,--j){
+            if(i==1){
+                pas[i][j]=1;
+                continue;
+            }
+            
+            for(int k=j ,jmp=i;jmp>0;k+=2,--jmp){
+                if(k==j) {
+                    pas[i][k]=1;
+                    continue;
+                }
+                 if(jmp==1){
+                    pas[i][k]=1;
+                  continue;
+
+                }
+                    pas[i][k]+=pas[i-1][k-1]+pas[i-1][k+1];
+            }
+            
+        }
+        for(int i=1,j=n;i<=n && j>=1;i++,--j){
+            if(i==1){
+               ans[i-1].push_back(pas[i][j]);
+                continue;
+            }
+            
+            for(int k=j, jmp=i;jmp>0;k+=2,--jmp){
+               ans[i-1].push_back(pas[i][k]);
+            }
+            
+        }
+        return ans;
+        
+        
+        
+    }
 };
