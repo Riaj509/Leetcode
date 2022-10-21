251e6d0fb3883e97258b49eda61dc20b9e6ae32e
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-21 18:21:51 +0600

    Time: 7 ms (61.12%), Space: 12.6 MB (31.87%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,27 +11,55 @@
  */
 class Solution {
 public:        
-    
-    
-
-    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> levelOrder(TreeNode* root){
+        
         vector<vector<int>>ans;
-
+       if(!root) return ans;
         
-        TreeNode *fs=root;
-        check(fs,0,ans);
+        queue<TreeNode*>q;
+   
+        q.push(root);
+        q.push(NULL); 
+        vector<int>cur;
+        while(!q.empty()){
+            TreeNode *tmp=q.front();
+            q.pop();
+            if(tmp==NULL){
+                ans.push_back(cur);
+                cur.resize(0);
+                if(q.size()>0){
+                  q.push(NULL);  
+                }
+            }
+            else{
+            
+                cur.push_back(tmp->val);
+                if(tmp->left) q.push(tmp->left);
+                if(tmp->right) q.push(tmp->right);
+            }
+        }
         return ans;
+        
     }
     
-    void check(TreeNode *root,int dept,vector<vector<int>>&a){
+     ///////using dfs/////
+//     vector<vector<int>> levelOrder(TreeNode* root) {
         
-        if(root==NULL) return ;       
-        if(a.size()==dept)a.push_back(vector<int>());
-        a[dept].push_back(root->val);
-        check(root->left,dept+1,a);
-        check(root->right,dept+1,a);
+//         vector<vector<int>>ans;
+//         TreeNode *fs=root;
+//         check(fs,0,ans);
+//         return ans;
+//     }
+    
+//     void check(TreeNode *root,int dept,vector<vector<int>>&a){
+        
+//         if(root==NULL) return ;       
+//         if(a.size()==dept)a.push_back(vector<int>());
+//         a[dept].push_back(root->val);
+//         check(root->left,dept+1,a);
+//         check(root->right,dept+1,a);
         
         
 
-    }
+//     }
 };
