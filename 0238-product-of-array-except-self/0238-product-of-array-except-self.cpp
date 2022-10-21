f44f521e9870b6643d8e653806a3d52af4aae6e4
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-21 16:20:07 +0600

    Time: 62 ms (22.10%), Space: 26.1 MB (5.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        int pref[n],suf[n];
-        pref[0]=nums[0];
-        suf[n-1]=nums[n-1];
-        for(int i=1;i<n;i++) pref[i]=pref[i-1]*nums[i];
-        for(int i=n-2;i>=0;i--) suf[i]=suf[i+1]*nums[i];
+        vector<pair<int,int>>v(n);
+        v[0].first=nums[0];
+        v[n-1].second=nums[n-1];
+        for(int i=1;i<n;i++) v[i].first=v[i-1].first*nums[i];
+        for(int i=n-2;i>=0;i--) v[i].second=v[i+1].second*nums[i];
         
         vector<int>ans;
         for(int i=0;i<n;i++){
-            if(i==0) ans.push_back(suf[i+1]);
-            else if(i==n-1)ans.push_back(pref[i-1]);
+            if(i==0) ans.push_back(v[i+1].second);
+            else if(i==n-1)ans.push_back(v[i-1].first);
             else {
-             ans.push_back(pref[i-1]*suf[i+1]);
+             ans.push_back(v[i-1].first*v[i+1].second);
             }
         }
         return ans;
