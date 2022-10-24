234d5fe2df43324711943f1c0633920535252815
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-24 12:51:58 +0600

    Time: 88 ms (10.92%), Space: 25 MB (76.32%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-        vector<int>deco;
-        deco.push_back(first);
-        deco.push_back(first^encoded[0]);
-        
-        for(int i=1;i<encoded.size();++i){
-            deco.push_back(encoded[i]^deco.back());
+        encoded.insert(encoded.begin(),first);
+        for(int i=1;i<encoded.size();i++)
+        {
+        encoded[i] = encoded[i] ^ encoded[i-1];
         }
-        return deco;
+        return encoded;
     }
 };
