0ac07c9a6dece3e954300fa6ee4e2ee237be9def
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-24 17:51:21 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (74.33%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int sm=0;
-        for(int i=0;i<32;i++){
-            if(start&(1<<i)^goal&(1<<i))sm++;
-        }
-        return sm;
+        
+        return __builtin_popcount(start^goal);
+        
     }
 };
