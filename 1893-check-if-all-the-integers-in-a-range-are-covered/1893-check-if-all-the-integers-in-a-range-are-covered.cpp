4736447ca183180559d578cf0834082391e8cf52
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-24 20:31:00 +0600

    Time: 8 ms (61.93%), Space: 9.4 MB (6.82%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,22 +1,16 @@
 class Solution {
-private:
-    //returns true if interval l contains interval r
-    bool contains(vector<int>& l, vector<int>& r) {
-        return l[0] <= r[0] && r[1] <= l[1];
-    }
 public:
     bool isCovered(vector<vector<int>>& ranges, int left, int right) {
-        sort(begin(ranges), end(ranges), [this](vector<int>& l, vector<int>& r) {
-            if (contains(r, l)) return false;
-            else if (contains(l, r)) return true;
-            else return l[0] < r[0];
-        });
-        int v = 0, k = left, n = ranges.size();
-        while (v < n && k <= right) {
-            if (v == 0 || !contains(ranges[v - 1], ranges[v]))
-                while (ranges[v][0] <= k && k <= ranges[v][1] && k <= right) k++;
-            v++;
+        int lft=left,ri=right;
+        set<int>st;
+        for(auto u : ranges){
+             for(int i=u[0];i<=u[1];++i){
+            st.insert(i);
         }
-        return k > right;
+        }
+        
+        for(int i=left;i<=right;++i) if(st.find(i)==st.end())return false;
+       
+        return true;
     }
 };
