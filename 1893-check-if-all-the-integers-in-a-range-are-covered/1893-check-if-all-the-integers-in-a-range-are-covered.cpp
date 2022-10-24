34efa6dad713592cc70d9d458bd4a494129c50b1
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-24 20:33:18 +0600

    Time: 10 ms (46.40%), Space: 8.9 MB (44.89%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
-    bool isCovered(vector<vector<int>>& ranges, int left, int right) {
-        int lft=left,ri=right;
-        set<int>st;
-        for(auto u : ranges){
-             for(int i=u[0];i<=u[1];++i){
-            st.insert(i);
-        }
-        }
+ 
+
+bool isCovered(vector<vector<int>>& vec, int left, int right) {
+    
+      sort(vec.begin(),vec.end());
+    int flag=0;
+
+    for(int j=left;j<=right;j++)
+    {
+        for(int i=0;i<vec.size();i++)
+            {
+               if(inRange(vec[i][0],vec[i][1],j))
+               {
+                   flag=1;
+                   break;
+               }
+            
+                if(flag)
+                    break;
+            }
         
-        for(int i=left;i<=right;++i) if(st.find(i)==st.end())return false;
-       
-        return true;
+        
+        if(flag==0)
+            return false;
+        else
+            flag=0;
     }
+   
+    
+    return true;
+}
+       bool inRange(int low, int high, int x)
+{
+return ((x-high)*(x-low) <= 0);
+}
 };
