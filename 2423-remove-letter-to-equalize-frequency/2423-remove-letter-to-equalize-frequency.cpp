98b74f37032a80cd119465e0bf4c484c4c48afe5
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-24 14:02:10 +0600

    Time: 0 ms (100.00%), Space: 6.3 MB (53.47%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
+    bool equalFrequency(string word) {
+        unordered_map<char, int> mp;
+        map<int, int> mp2;
+        for(auto c: word)  mp[c]++;
         
-    bool solve(unordered_map<char,int> &m){
-		set<int>st;
-		for(auto it:m){
-			if(it.second) st.insert(it.second);
-		}
-		if(st.size()>1) return false;
-		return true;
-	}
-	bool equalFrequency(string word) {
-		unordered_map<char,int>mpp;
-
-		for(char c:word) mpp[c]++;
-
-		for(char c:word){
-			mpp[c]--;
-			if(solve(mpp)) return true;
-			mpp[c]++;
-		}
-		return false;
-	}
+        for(auto m: mp)  mp2[m.second]++;
+        
+        
+        if(mp2.size() > 2) return false;
+        map<int, int>::iterator it1 = mp2.begin();
+        map<int, int>::iterator it2 = mp2.begin();
+        it2++;
+        
+        if(mp2.size() == 1){
+            if(mp.size() == 1 || it1->first == 1) return true;
+            return false;
+        }
+       
+        
+        if(it1->first == 1 && it1->second == 1) return true;
+        if(it1->first == it2->first-1 && it2->second == 1) return true;
+        return false;
+    }
 };
