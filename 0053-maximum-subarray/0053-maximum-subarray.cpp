78958db1f2f1334b2902940ced1ab84003e9639e
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-27 18:06:52 +0600

    Time: 196 ms (65.15%), Space: 67.8 MB (11.45%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,20 +3,31 @@ public:
     int maxSubArray(vector<int>& nums) {
         
         int n=nums.size();
-        int cur_sm=0,mx_so_far=INT_MIN;
-        for(int i=0;i<n;++i){
-            cur_sm+=nums[i];
-            
-            
-            if(cur_sm>mx_so_far){
-                mx_so_far=cur_sm;
-            }
-            if(cur_sm<0){
-                cur_sm=0;
-            }
-            
+        
+         return check(0,n-1,nums);
+    }
+    
+    
+    int check(int l,int r,vector<int>&nums){
+        
+        if(l>r) return INT_MIN;
+        
+        int mid=(l+r)>>1;
+        
+        int mx_l=check(l,mid-1,nums);
+        int mx_r=check(mid+1,r,nums); 
+        
+        int l_sm=0,r_sm=0,sm=0;
+        for(int i=mid-1;i>=l;--i){
+            sm+=nums[i];
+            l_sm=max(l_sm,sm);
+        }
+        sm=0;
+         for(int i=mid+1;i<=r;i++){
+            sm+=nums[i];
+            r_sm=max(r_sm,sm);
         }
         
-        return mx_so_far;
+        return max({mx_l,mx_r,l_sm+r_sm+nums[mid]});
     }
 };
