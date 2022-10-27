a973f659e205700548beaf652e3764b7446cfe13
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-27 21:15:28 +0600

    Time: 135 ms (65.36%), Space: 48.4 MB (44.46%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,7 +4,6 @@ public:
         if(nums.size()==1) return true;
         int sm=0;
         for(int i=0;i<nums.size()-1;++i){
-            
             if(nums[i]==0 && sm==i) return false;
             sm=max(sm,nums[i]+i);
         }
