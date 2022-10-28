b557f9e8bd0442956a244cdad2cef3dee5483656
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-10-28 20:08:48 +0600

    Time: 32 ms (78.62%), Space: 9.2 MB (67.99%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        
+        
+        vector<vector<int>>ans;
+        vector<int>tmp;
+        check(1,n,k,ans,tmp);
+        
+        return ans;
+    }
+    
+    void check(int str,int end,int k,vector<vector<int>>&ans,vector<int>&tmp){
+        
+        if(k==0){
+            ans.push_back(tmp);
+            return;
+        }
+        
+        for(int i=str;i<=end;++i){
+            if(k){
+                tmp.push_back(i);
+                check(i+1,end,k-1,ans,tmp);
+                
+                tmp.pop_back();///back_track///
+            }
+        }
+    }
+};
