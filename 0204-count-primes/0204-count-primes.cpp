699d9ffd7b40617039ad81e8c390110db8b70d28
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-11-04 00:02:20 +0600

    Time: 226 ms (82.84%), Space: 10.2 MB (68.82%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int countPrimes(int n) {
-        
-        
-        vector<bool>vis(n+1,false);
-        int ans=0;
-        for(int i=2;i<n;i++){
-            if(vis[i])continue;
-            ++ans;
-            for(long long j=i;j<n;j+=i){
-                vis[j]=true;
-            }
-        }
-        return ans;
-    }
+	int countPrimes(int n) {
+		//prime no. generation using sieve of eratothons
+		vector<bool> prime(n + 1, true);
+		prime[0] = false;
+		prime[1] = false;
+		for (int i = 2; i * i <= n; i++) {
+			if (prime[i]) {
+				for (int j = i * i; j <= n; j += i) {
+					prime[j] = false;
+				}
+			}
+		}
+		//counting prime numbers
+		int primeCount = 0;
+		for (int i = 2; i < n; i++) {
+			if (prime[i]) {
+				primeCount++;
+			}
+		}
+		return primeCount;
+	}
 };
