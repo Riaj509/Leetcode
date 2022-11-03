616790e430af4b04a9a2e2aa5795ea7f76ae1cd7
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-11-03 17:32:46 +0600

    Time: 2 ms (45.02%), Space: 6 MB (22.91%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -5,7 +5,15 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         
-        if(isBadVersion(n)) return firstBadVersion(n-1);
-        else return n+1;
+      int l=1,r=n;
+        while(l<r){
+            int mid=(l+(r-l)/2);
+            if(isBadVersion(mid)){
+                r=mid;
+            }
+            else l=mid+1;
+        }
+        return l;
     }
+    
 };
