75ba6e7f8b459ca67320af22ceb71e97a5d28551
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-11-03 22:13:56 +0600

    Time: 40 ms (49.57%), Space: 11.2 MB (93.78%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,19 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int findPairs(vector<int>& nums, int k) {
+    int min_d=INT_MAX,val=-1;
+    int getMinimumDifference(TreeNode* root) {
         
-        int pair=0;
         
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<nums.size();++i){
-            if(i>0 && nums[i]==nums[i-1])continue;
-            int need=nums[i]+k;
-            int ln=lower_bound(nums.begin()+i+1,nums.end(),need)-nums.begin();
-            if(ln>i && ln<nums.size() && nums[ln]==need) pair++;
-        }
-        
-        return pair;
-        
+        if(root->left!=NULL) getMinimumDifference(root->left);
+        if(val>=0)
+         min_d=min(min_d,abs((root->val)-val));
+         val=root->val;
+        if(root->right!=NULL) getMinimumDifference(root->right);
+                   
+        return min_d;
+                                   
     }
 };
