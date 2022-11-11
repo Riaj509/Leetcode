54de056d84dc0224b949de97b29a116ac040c774
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-11-11 19:21:08 +0600

    Time: 18 ms (93.52%), Space: 11 MB (48.05%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -6,7 +6,7 @@ public:
         string ans;
        for(int i=0;i<s.size();++i){
            if(ans.back()==s[i])ans.pop_back();
-           else ans+=s[i];
+           else ans.push_back(s[i]);
            
        }
         
