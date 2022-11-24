2d8c7c75ecd4e6a0a87ef31d567d4c91e85b3ad3
Riaj509 <70841491+Riaj509@users.noreply.github.com>
2022-11-24 14:57:07 +0600

    Time: 586 ms (6.57%), Space: 109.7 MB (63.84%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -4,14 +4,12 @@ public:
         
         
         int sm=0,cur=0;
-        vector<int>ans(nums.size());
-        ans[0]=nums[0];
         for(int i=1;i<nums.size();i++){
-            ans[i]=ans[i-1]+nums[i];
+            nums[i]+=nums[i-1];
         }
-        sm=ans[k-1];
+        sm=nums[k-1];
         for(int i=k;i<nums.size();i++){
-            cur=(ans[i]-ans[i-k]);
+            cur=(nums[i]-nums[i-k]);
             sm=max(cur,sm);
         }
         return (double) sm/k;
